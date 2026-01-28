3cd552a93fc7d4ebe13047d1da1c4d36dcddd8f0
rushimore17 <rushi2130@gmail.com>
2026-01-28 23:28:48 +0530

    Update SecondLargestBy_Bubble_sort.java

--- Question/SecondLargestBy_Bubble_sort.java
+++ Question/SecondLargestBy_Bubble_sort.java
@@ -4,20 +4,41 @@ public class BubbleSort {
         int n = arr.length;
 
         // Bubble sort ascending
-        for (int i = 0; i < n-1; i++) {
-            for (int j = 0; j < n-i-1; j++) {
-                if (arr[j] > arr[j+1]) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     // swap
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
 
+        // Print sorted array
         System.out.println("Ascending order:");
         for (int num : arr) {
             System.out.print(num + " ");
         }
+        System.out.println();
+
+        // Find second largest distinct element
+        if (n < 2) {
+            System.out.println("Second largest: -1");
+            return;
+        }
+
+        int largest = arr[n - 1];
+        int secondLargest = -1;
+
+        // Traverse backward to find distinct second largest
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < largest) {
+                secondLargest = arr[i];
+                break;
+            }
+        }
+
+        System.out.println("Second largest: " + secondLargest);
     }
 }
